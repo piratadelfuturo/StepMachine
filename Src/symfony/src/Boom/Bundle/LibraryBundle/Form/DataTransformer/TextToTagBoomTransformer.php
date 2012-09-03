f47fe5a9f4c98f6267da7e6d668d61eaf2a4aaf8
Daniel Maldonado <daniel.maldonado@gmail.com>
2012-09-03 13:20:14 -0500

    tag transformer

--- Src/symfony/src/Boom/Bundle/LibraryBundle/Form/DataTransformer/TextToTagBoomTransformer.php
+++ Src/symfony/src/Boom/Bundle/LibraryBundle/Form/DataTransformer/TextToTagBoomTransformer.php
@@ -4,7 +4,7 @@ namespace Boom\Bundle\LibraryBundle\Form\DataTransformer;
 use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\Exception\TransformationFailedException;
 use Doctrine\Common\Persistence\ObjectManager;
-use Boom\Bundle\LibraryBundle\;
+use Boom\Bundle\LibraryBundle\Entity\Tag;
 
 /**
  * Description of TextToTagBoomTransformer
@@ -13,5 +13,27 @@ use Boom\Bundle\LibraryBundle\;
  */
 class TextToTagBoomTransformer implements DataTransformerInterface
 {
+
+    /**
+     * @var ObjectManager
+     */
+    private $om;
+
+    /**
+     * @param ObjectManager $om
+     */
+    public function __construct(ObjectManager $om)
+    {
+        $this->om = $om;
+    }
+
+    public function reverseTransform($value) {
+
+    }
+
+
+    public function transform($value) {
+        ;
+    }
 
 }
