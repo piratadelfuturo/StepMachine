2f2e8a9c5f33d4bf0690331d9a68dfb8b414f839
Eder <eder@brutalcontent.com>
2012-09-06 14:54:42 -0500

    more flag styles

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Default/blocks/big_top.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Default/blocks/big_top.html.php
@@ -1,22 +1,22 @@
 <div id="main-car">
-      <a href="#" class="car-btn prev">prev</a>
-      <a href="#" class="car-btn next">next</a>
-      <div class="active cf">
-        <div class="img-container">
-          <img src="http://placehold.it/677x382" class="main-img" alt="placeholder"/>
-        </div>
-        <h2>Los 7 mejores discos del 2012</h2>
-        <p>Everyone wants to be a world-record holder. Now Toyota Prius and URDB are bringing you the chance. Join us on March 30 and 31 right here for a live, two-day, record-setting event! Everyone wants to be a world-record holder. Now Toyota Prius and URDB are bringing you the chance. Join us on March 30 and 31 right here for a live, two-day, record-setting event!</p>
-      </div>
-      <div id="carousel">
-        <ul>
-          <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
-          <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
-          <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
-          <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
-          <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
-          <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
-          <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>          
-        </ul>
-      </div>
+  <a href="#" class="car-btn prev">prev</a>
+  <a href="#" class="car-btn next">next</a>
+  <div class="active cf">
+    <div class="img-container">
+      <img src="http://placehold.it/677x382" class="main-img" alt="placeholder"/>
     </div>
+    <h2>Los 7 mejores discos del 2012</h2>
+    <p>Everyone wants to be a world-record holder. Now Toyota Prius and URDB are bringing you the chance. Join us on March 30 and 31 right here for a live, two-day, record-setting event! Everyone wants to be a world-record holder. Now Toyota Prius and URDB are bringing you the chance. Join us on March 30 and 31 right here for a live, two-day, record-setting event!</p>
+  </div>
+  <div id="carousel">
+    <ul>
+      <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
+      <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
+      <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
+      <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
+      <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
+      <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>
+      <li><a href="#"><p>Come caca</p><img src="http://placehold.it/133x75" alt="placeholder" /></a></li>          
+    </ul>
+  </div>
+</div>
