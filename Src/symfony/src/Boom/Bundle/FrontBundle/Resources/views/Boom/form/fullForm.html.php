7a8b2f2373cb2c5d4fba957ee9daad6779a18f70
Cas <cas@brutalcontent.com>
2012-09-12 14:41:34 -0500

    form crear boom, tabs on n shit

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Boom/form/fullForm.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Boom/form/fullForm.html.php
@@ -15,70 +15,69 @@
         ?>
         <section>
             <?php echo $view['form']->label($form['image'], 'Imagen') ?>
-            <div><?php echo $view['form']->widget($form['image'], array('attr' => array('class' => 'image-uploader'))); ?> </div>
+            <div><?php echo $view['form']->widget($form['image'], array('attr' => array('class' => 'image-uploader'))); ?></div>
         </section>
         <?php echo $view['form']->row($form['nsfw'], array('label' => 'NSFW')) ?>
         <?php echo $view['form']->row($form['category'], array('label' => 'Categoría principal')) ?>
     </fieldset>
-    <fieldset id="<?php echo $form['elements']->get('id') ?>" class="sort-elements booms">
-        <label>Boomies</label>
-        <?php
-        foreach ($form['elements'] as $element):
+    <div id="<?php echo $form['elements']->get('id') ?>" class="sort-elements booms">
+        <?php foreach ($form['elements'] as $element):?>
+        <fieldset id="<?php echo $element->get('id') ?>" class="boomie boom">
+          <form>
+            <label>
+              <span class="place"><?php echo "{$element['position']->vars['value']}"; ?></span>
+            </label>
+            <input class="up-pic" type="file multiple" name="boom-pic" placeholder="Arrastra tu foto para agregarla."> 
+            <?php echo $element['title']->vars['value'] ?>
+            <?php echo $view['form']->widget(
+                $element['title'], array(
+                  'attr' => array(
+                  'class' => 'boomie-title-input',
+                  'placeholder' => 'Título del Boom No.'.$element['position']->vars['value']
+                )
+              )
+            );?>
+          </form>
+          <div class="accordion_content">
+            <?php if (isset($element['id']) && $element['id'] !== null): ?>
+              <?php //echo $view['form']->widget($element['id']); ?>
+            <?php endif; ?>
+            <ul class="wyswyg-menu">
+              <a href="#"><li class="hyperlink">Hipervinculo</li></a>
+              <a href="#"><li class="picture">Foto</li></a>
+              <a href="#"><li class="embed">Embed</li></a>
+              <a href="#"><li class="gallery">Galería</li></a>
+              <a href="#"><li class="video">Video</li></a>
+            </ul>
+            <?php
+            echo $view['form']->widget(
+              $element['position'], array(
+                'attr' => array(
+                  'class' => 'boomie-position-input'
+                )
+              )
+            );
+            echo $view['form']->widget(
+              $element['content'], array(
+                'attr' => array(
+                  'class' => 'boom-wysiwyg',
+                  'placeholder' => 'Escribe aquí tu boom...'
+                )
+              )
+            );
             ?>
-            <div id="<?php echo $element->get('id') ?>" class="boomie">
-                <label>
-                    <span class="place"><?php echo "{$element['position']->vars['value']}"; ?></span>
-                </label>
-                <input class="up-pic" type="file multiple" name="boom-pic" placeholder="Arrastra tu foto para agregarla."> 
-                <?php echo $element['title']->vars['value'] ?>
-                <?php echo $view['form']->widget(
-                      $element['title'], array(
-                        'attr' => array(
-                          'class' => 'boomie-title-input',
-                          'placeholder' => 'Título del Boom No.'.$element['position']->vars['value']
-                        )
-                      )
-                    );?>
-                  <div class="accordion_content">
-                    <?php if (isset($element['id']) && $element['id'] !== null): ?>
-                        <?php //echo $view['form']->widget($element['id']); ?>
-                    <?php endif; ?>
-                    <ul class="wyswyg-menu">
-                      <a href="#"><li class="hyperlink">Hipervinculo</li></a>
-                      <a href="#"><li class="picture">Foto</li></a>
-                      <a href="#"><li class="embed">Embed</li></a>
-                      <a href="#"><li class="gallery">Galería</li></a>
-                      <a href="#"><li class="video">Video</li></a>
-                    </ul>
-                    <?php
-                    echo $view['form']->widget(
-                            $element['position'], array(
-                        'attr' => array(
-                            'class' => 'boomie-position-input'
-                        )
-                            )
-                    );
-                    echo $view['form']->widget(
-                            $element['content'], array(
-                        'attr' => array(
-                          'class' => 'boom-wysiwyg',
-                          'placeholder' => 'Escribe aquí tu boom...'
-                        )
-                            )
-                    );
-                    ?>
-                </div>
-            </div>
-            <span class="tab"><a href=""><span>TAB</span></a></span>
+          </div>
+          <span class="tab"><a href=""><span>TAB</span></a></span>
+        </fieldset>
         <?php endforeach; ?>
-        <?php echo $view['form']->row(
-          $form['tags'], array(
-            'attr' => array(
-              'label' => 'Tags:',
-              'placeholder' => 'Escribe tus tags separados por comas...'
-            )
+      </div>
+      <?php echo $view['form']->row(
+        $form['tags'], array(
+          'attr' => array(
+            'label' => 'Tags:',
+            'placeholder' => 'Escribe tus tags separados por comas...'
           )
-        ) ?>
-    </fieldset>
+        )
+      ) ?>
     <button class="submit" type="submit" >¡Publicar!</button>
 </form>
