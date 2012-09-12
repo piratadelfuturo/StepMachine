f910880677ce555670a08528c84081dd996f6def
Daniel Maldonado <daniel.maldonado@gmail.com>
2012-09-12 18:22:59 +0200

    Patch para home vacio

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Default/blocks/big_top.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Default/blocks/big_top.html.php
@@ -1,9 +1,15 @@
 <div id="main-car">
     <a href="#" class="car-btn prev">prev</a>
     <a href="#" class="car-btn next">next</a>
-    <?php foreach ($list as $element): ?>
-    <?php $image = isset($element['image']['path']) ? $element['image']['path'] : 'http://placekitten.com/g/677/381'?>
-        <div class="active cf">
+    <?php
+    $position = 0;
+    if(!isset($list) || ($list === null || empty($list))){
+        $list = array();
+    }
+    foreach ($list as $element):
+        $position++;
+        $image = isset($element['image']['path']) ? $element['image']['path'] : 'http://placekitten.com/g/677/381'?>
+        <div class="<?php echo $position == 1 ? 'active' : '' ?> cf">
             <div class="img-container">
                 <img src="<?php echo $image ?>" class="main-img" alt="<?php echo $view->escape($element['title']) ?>"/>
             </div>
