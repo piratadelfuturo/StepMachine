611aa1519fa9b9825a060915f997c8913d0103da
Eder <eder@brutalcontent.com>
2012-09-14 17:40:22 -0500

    password reset style

--- Src/symfony/src/Boom/Bundle/UserBundle/Resources/views/Resetting/reset_content.html.php
+++ Src/symfony/src/Boom/Bundle/UserBundle/Resources/views/Resetting/reset_content.html.php
@@ -1,7 +1,16 @@
-<form action="<?php echo $view['router']->generate('fos_user_resetting_reset',array('token' => $token)); ?>"
+<?php $view->extend('BoomFrontBundle::two_col_sublayout.html.php') ?>
+<div class="pass-page">
+  <h3 class="short-bar">Recuperar Password</h3>
+  <p class="lab">Ok, ya vimos que no te gustó tu antigua contraseña, <br />
+    ahora sólo escribe la nueva un par de veces.</p>
+  <form action="<?php echo $view['router']->generate('fos_user_resetting_reset',array('token' => $token)); ?>"
       <?php echo $view['form']->enctype($form) ?> method="POST" class="fos_user_resetting_reset">
-    <?php echo $view['form']->widget($form) ?>
-    <div>
-        <input type="submit" value="Reset" />
-    </div>
-</form>
+      <div class="grad-border">
+        <?php echo $view['form']->widget($form) ?>
+      </div>
+
+      <div>
+          <input type="submit" value="Reset" />
+      </div>
+  </form>
+</div>
