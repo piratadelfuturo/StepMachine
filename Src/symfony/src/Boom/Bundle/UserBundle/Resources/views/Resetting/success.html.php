8ddbeae43a55d3524b3259b7b31de36f4317cb5e
Eder <eder@brutalcontent.com>
2012-09-19 14:00:53 -0500

    successfull password change message

---
+++ Src/symfony/src/Boom/Bundle/UserBundle/Resources/views/Resetting/success.html.php
@@ -0,0 +1,6 @@
+<?php $view->extend('BoomFrontBundle::two_col_sublayout.html.php') ?>
+
+<div class="pass-page">
+  <h3 class="short-bar">Recuperar Password</h3>
+  <p class="lab">Listo, tu contraseña ya cambió.</p>
+</div>
