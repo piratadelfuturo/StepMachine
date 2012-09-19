f9b4584d45fdc250c871d65121e35715ba1caff5
Eder <eder@brutalcontent.com>
2012-09-19 10:45:53 -0500

    login NOT OK

--- Src/symfony/src/Boom/Bundle/UserBundle/Resources/views/Security/login.html.php
+++ Src/symfony/src/Boom/Bundle/UserBundle/Resources/views/Security/login.html.php
@@ -1,22 +1,23 @@
 <?php $view->extend('BoomFrontBundle::two_col_sublayout.html.php') ?>
 
-<div class="user-login">
+<div class="user-login pass-page">
 
 <?php if(isset($error)): ?>
   <div class="box-error"><?php echo $error; ?></div>
 <?php endif; ?>
 
-<pre>
-</pre>
-
-
-<form action="" method="post">
-  <input type="hidden" name="_csrf_token" value="<?php echo $csrfToken ?>" />
-  <input type="text" id="username" name="_username" value="<?php echo $lastUsername ?>" />
-  <input type="password" id="password" name="_password" placeholder="Password" />
-
-  <input type="checkbox" id="remember_me" name="_remember_me" value="on" />
-  <label for="remember_me">Recordar</label>
+<form action="login_check" method="post">
+  <input type="hidden" name="_csrf_token" value="<?php echo $csrf_token ?>" />
+  <div class="grad-border">
+    <input type="text" id="username" name="_username" placeholder="Cuenta de usuario" value="<?php echo $last_username ?>" />
+  </div>
+  <div class="grad-border">
+    <input type="password" id="password" name="_password" placeholder="Password" />
+  </div>
+  <div class="rem-container">
+    <input type="checkbox" id="remember_me" name="_remember_me" value="on" />
+    <label for="remember_me" class="lab">Recordar contraseña</label>
+  </div>
   <input type="submit" id="_submit" name="_submit" value="Enviar" />
 </form>
 
