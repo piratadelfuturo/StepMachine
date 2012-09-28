ccd2c0c20b0cb684d336ddc0aaadcb3d7d21ea56
Cas <cas@brutalcontent.com>
2012-09-28 16:29:07 -0500

    galerias inline

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Gallery/inline.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Gallery/inline.html.php
@@ -1,7 +1,36 @@
-<ul>
-    <?php foreach ($entity['galleryimagerelations'] as $image): ?>
-        <li>
-            <img src="<?php echo $view['boom_image']->getBoomImageUrl($image['image']['path'],158,90); ?>" />
-        </li>
+<div id="main-car" class="gal-car">
+  <div class="car-nav">
+    <a href="#" class="gal-btn prev">prev</a>
+    <a href="#" class="gal-btn next">next</a>
+  </div>
+  <div class="slide-container cf">
+    <?php
+      $position = 0;
+      $gallerySize = sizeof($entity['galleryimagerelations']);
+      foreach ($entity['galleryimagerelations'] as $image):
+            $position++;
+    ?>
+    <div class="slide <?php echo $position == 1 ? 'active' : '' ?> cf">
+      <img src="<?php echo $view['boom_image']->getBoomImageUrl($image['image']['path'],680,382); ?>" />
+    </div>
     <?php endforeach; ?>
-</ul>
+  </div>
+  <?php if ($gallerySize > 7):?>
+  <div class="gal-thumbs">
+      <ul>
+      <?php
+        $position = 0;
+      foreach ($entity['galleryimagerelations'] as $image):
+          $position++;
+      ?>
+      <li class="<?php echo $position == 1 ? 'active' : '' ?>">
+        <img src="<?php echo $view['boom_image']->getBoomImageUrl($image['image']['path'],133,75); ?>"/>
+      </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+  <?php endif;?>
+</div>
+
+
+
