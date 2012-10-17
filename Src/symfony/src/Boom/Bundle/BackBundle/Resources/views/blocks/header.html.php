f3fffc8a16884b866453677b2b93409080832f4b
Daniel M <daniel.maldonado@gmail.com>
2012-10-17 14:04:58 +0400

    fix para flash messages

--- Src/symfony/src/Boom/Bundle/BackBundle/Resources/views/blocks/header.html.php
+++ Src/symfony/src/Boom/Bundle/BackBundle/Resources/views/blocks/header.html.php
@@ -10,19 +10,25 @@ $User = $app->getUser();
             <li>
                 <ul>
                     <li><a href="#"><?php echo $User->getUsername() ?></a>
-                        <?php $score = rand(2,11)?>
+                        <?php $score = rand(2, 11) ?>
                         <span><?php echo $score ?></span>
                         <ul>
                             <li><a href="#">Score: <?php echo $score ?></a></li>
                         </ul>
                     </li>
-                    <?php if ($view['session']->hasFlash('notice')): ?>
-                    <li>
-                        <div class="alert note">
-                        <?php echo $view->escape($view['session']->getFlash('notice')); ?>
-                        </div>
-                    </li>
-                    <?php endif; ?>
+                    <?php
+                    if ($view['session']->hasFlash('notice')):
+                        foreach ($view['session']->getFlash('notice') as $message):
+                            ?>
+                            <li>
+                                <div class="alert note">
+                                    <?php echo $view->escape(); ?>
+                                </div>
+                            </li>
+                            <?php
+                        endforeach;
+                    endif;
+                    ?>
                 </ul>
             </li>
         </ul>
