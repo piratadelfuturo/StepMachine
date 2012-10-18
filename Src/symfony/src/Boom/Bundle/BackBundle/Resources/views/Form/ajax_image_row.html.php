0531167d9f09d6c5ed637729f66364aee7613330
Daniel M <daniel.maldonado@gmail.com>
2012-10-19 03:01:54 +0400

    Fix para subir boom

--- Src/symfony/src/Boom/Bundle/BackBundle/Resources/views/Form/ajax_image_row.html.php
+++ Src/symfony/src/Boom/Bundle/BackBundle/Resources/views/Form/ajax_image_row.html.php
@@ -5,8 +5,6 @@
     <div>
       <?php
       echo $view['form']->errors($form);
-      //var_dump(get_defined_vars());
-      //exit;
       ?>
       <img id="<?php echo $parameters['id'].'_img' ?>"src="<?php echo $data['path'] === null ? '' : $view['boom_image']->getBoomImageUrl($data['path'],158,90)?>" />
       <?php echo $view['form']->widget($form['id']) ?>
--- Src/symfony/src/Boom/Bundle/BackBundle/Resources/views/Form/field_errors.html.php
+++ Src/symfony/src/Boom/Bundle/BackBundle/Resources/views/Form/field_errors.html.php
@@ -1,3 +1,4 @@
+<?php var_dump($errors);?>
 <?php if($errors):?>
 <div class="alert info">
 <?php echo $view['form']->block($form, 'form_errors') ?>
