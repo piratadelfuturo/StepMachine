193762589d2f0209ac1e3125b63e00b8e038a09a
Cas <cas@brutalcontent.com>
2012-10-22 14:37:45 -0500

    bloques de abajo del widget II

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Boom/blocks/userOrder.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Boom/blocks/userOrder.html.php
@@ -9,6 +9,7 @@ $userElements = array();
 ?>
 
 <div id="usr-booms">
+    <div class="shade">shade</div>
     <div class="botones">
         <a href="#" class="on" id="tendencia">Tendencias</a>
         <span class="divider">divider</span>
@@ -17,7 +18,7 @@ $userElements = array();
     </div>
     <div class="big-container cf">
         <div class="dyna-content tend-cont on">
-            <h3>nuestros usuarios dicen</h3>
+            <h3>Esto es lo que nuestros usuarios dicen: ¿estás de acuerdo? ¡Edítalo!</h3>
             <ul class="drag-booms" reorder-url="<?php echo $view['router']->generate(
                         'BoomFrontBundle_boom_reorder',
                         array(
@@ -42,7 +43,7 @@ $userElements = array();
             </ul>
         </div>
         <div class="dyna-content miboom-cont">
-            <h3>Mi Boom</h3>
+            <h3>Tú dices</h3>
             <ul class="drag-booms">
                 <li>
                     <div class="balloon">
@@ -78,5 +79,24 @@ $userElements = array();
             </ul>
         </div>
     </div>
-    <a href="#" id="editalo"><span>Guardar</span></a>
+    <a href="#" class="editalo">Guardar</a>
+    <div class="info-blocks">
+      <div class="share-boom">
+        <h3>¿Quieres compartir tu Boom?</h3>
+        <form>
+          <input type="checkbox" class="select twitter"><label for="twitter"><span>twitter</span></label>
+          <input type="checkbox" class="select facebook"><label for="facebook"><span>facebook</span></label>
+        </form>
+       <a href="#" class="grey-btn">Compartir</a>
+      </div>
+      <div class="sign-in">
+        <h3>¡Alto ahí!</h3>
+        <p>Nos encantó cómo quedó tu boom, pero para guardarlo, compartir y usar todos nuestros ósom features, debes registrarte primero.</p>
+        <a href="#" class="grey-btn">Regístrate</a>
+      </div>
+      <div class="boom-clean">
+        <p>Aún no has editado <span>este boom.</span> Arrastra íconos, modifícalo, opina para que éste sea <span>tu boom.</span></p>
+        <a href="#" class="editalo">¡Edítalo!</a>
+      </div>
+  </div>
 </div>
