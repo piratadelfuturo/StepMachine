677ef6fc99ab82c788ffa0a631f4b6f81da80daa
Cas <cas@brutalcontent.com>
2012-10-25 10:52:48 -0500

    profile II

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/User/profile.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/User/profile.html.php
@@ -1,19 +1,134 @@
 <?php $view->extend('BoomFrontBundle::two_col_sublayout.html.php') ?>
 <?php $view['slots']->set('layout_container_css_class','colaboradores'); ?>
-
 <div class="profile">
   <div class="author-profile">
     <h3 class="title-flag">Perfil</h3>
     <div class="author-info cf">
       <img src="http://placekitten.com/147/147">
       <h4>Phillip K. Dick</h4><p class="boom-n">(20 Booms)</p>
-      <ul>
+      <ul class="author-links">
         <li><a href="#" class="btn-fb">facebook</a><a href="#">Phillip K. Dick</a></li>
         <li><a href="#" class="btn-tw">twitter</a><a href="#">@PhillipKDick</a></li>
       </ul>
       <a href="#" class="seguir">seguir</a>
     </div>
     <p class="author-bio">Phillip k. Dick es un escritor de California. Su literatura está influenciada por la narrativa policiaca de Raymond Chandler y los textos futuristas de William Gibson. K. Dick es quizá el autor más adaptado al cine (después de julio Verne, o Shakespeare, la verdad no sé, no investigué nada para este texto) con títulos como Blade Runner, Minority Report, On Call y muchas otras películas mierda basadas en libros geniales y bien escritos.</p>
+    <div class="boomer profile-booms">
+      <div class="title-flag">
+        <h3>Booms</h3>
+        <ul class="display-menu">
+          <li class="disp on"><a href="#">Creados</a></li>
+          <li class="disp"><a href="#">Modificados</a></li>
+          <li class="arrange"><a href="#" class="disp-list">lista</a></li>
+          <li class="arrange off"><a href="#" class="disp-mosaico">mosaico</a></li>
+        </ul>
+      </div>
+      <ul class="list cf">
+        <li class="boom on">
+          <img src="http://escenaelectoral.dev/content/boom-img/1c0b5f95/158_90.jpeg" width="158px" height="90px">
+          <div class="boom-info">
+            <span class="sm-flag musica">Musica</span>
+            <p class="boom-ti">
+              <a href="/musica/otro-boom-nuevo-pa-variar" class="boom-moar">Otro boom nuevo pa variar</a>
+            </p>
+            <a href="#" class="boom-moar">Leer Boom</a>
+          </div>
+          <p class="boom-info-mas">Publicado el <date>Mon, 08 Oct 12</date> - <span>234</span> veces visto - <span>10</span> comments - <span>3</span> modificaciones</p>
+        </li>
+        <li class="boom on">
+          <img src="http://escenaelectoral.dev/content/boom-img/1c0b5f95/158_90.jpeg" width="158px" height="90px">
+          <div class="boom-info">
+            <span class="sm-flag musica">Musica</span>
+            <p class="boom-ti">
+              <a href="/musica/otro-boom-nuevo-pa-variar" class="boom-moar">Otro boom nuevo pa variar</a>
+            </p>
+            <a href="#" class="boom-moar">Leer Boom</a>
+          </div>
+          <p class="boom-info-mas">Publicado el <date>Mon, 08 Oct 12</date> - <span>234</span> veces visto - <span>10</span> comments - <span>3</span> modificaciones</p>
+        </li>
+        <li class="boom on">
+          <img src="http://escenaelectoral.dev/content/boom-img/1c0b5f95/158_90.jpeg" width="158px" height="90px">
+          <div class="boom-info">
+            <span class="sm-flag musica">Musica</span>
+            <p class="boom-ti">
+              <a href="/musica/otro-boom-nuevo-pa-variar" class="boom-moar">Otro boom nuevo pa variar</a>
+            </p>
+            <a href="#" class="boom-moar">Leer Boom</a>
+          </div>
+          <p class="boom-info-mas">Publicado el <date>Mon, 08 Oct 12</date> - <span>234</span> veces visto - <span>10</span> comments - <span>3</span> modificaciones</p>
+        </li>
+        <li class="boom on">
+          <img src="http://escenaelectoral.dev/content/boom-img/1c0b5f95/158_90.jpeg" width="158px" height="90px">
+          <div class="boom-info">
+            <span class="sm-flag musica">Musica</span>
+            <p class="boom-ti">
+              <a href="/musica/otro-boom-nuevo-pa-variar" class="boom-moar">Otro boom nuevo pa variar</a>
+            </p>
+            <a href="#" class="boom-moar">Leer Boom</a>
+          </div>
+          <p class="boom-info-mas">Publicado el <date>Mon, 08 Oct 12</date> - <span>234</span> veces visto - <span>10</span> comments - <span>3</span> modificaciones</p>
+        </li>
+        <li class="boom on">
+          <img src="http://escenaelectoral.dev/content/boom-img/1c0b5f95/158_90.jpeg" width="158px" height="90px">
+          <div class="boom-info">
+            <span class="sm-flag musica">Musica</span>
+            <p class="boom-ti">
+              <a href="/musica/otro-boom-nuevo-pa-variar" class="boom-moar">Otro boom nuevo pa variar</a>
+            </p>
+            <a href="#" class="boom-moar">Leer Boom</a>
+          </div>
+          <p class="boom-info-mas">Publicado el <date>Mon, 08 Oct 12</date> - <span>234</span> veces visto - <span>10</span> comments - <span>3</span> modificaciones</p>
+        </li>
+        <li class="boom on">
+          <img src="http://escenaelectoral.dev/content/boom-img/1c0b5f95/158_90.jpeg" width="158px" height="90px">
+          <div class="boom-info">
+            <span class="sm-flag musica">Musica</span>
+            <p class="boom-ti">
+              <a href="/musica/otro-boom-nuevo-pa-variar" class="boom-moar">Otro boom nuevo pa variar</a>
+            </p>
+            <a href="#" class="boom-moar">Leer Boom</a>
+          </div>
+          <p class="boom-info-mas">Publicado el <date>Mon, 08 Oct 12</date> - <span>234</span> veces visto - <span>10</span> comments - <span>3</span> modificaciones</p>
+        </li>
+        <li class="boom on">
+          <img src="http://escenaelectoral.dev/content/boom-img/1c0b5f95/158_90.jpeg" width="158px" height="90px">
+          <div class="boom-info">
+            <span class="sm-flag musica">Musica</span>
+            <p class="boom-ti">
+              <a href="/musica/otro-boom-nuevo-pa-variar" class="boom-moar">Otro boom nuevo pa variar</a>
+            </p>
+            <a href="#" class="boom-moar">Leer Boom</a>
+          </div>
+          <p class="boom-info-mas">Publicado el <date>Mon, 08 Oct 12</date> - <span>234</span> veces visto - <span>10</span> comments - <span>3</span> modificaciones</p>
+        </li>
+        <li class="boom on">
+          <img src="http://escenaelectoral.dev/content/boom-img/1c0b5f95/158_90.jpeg" width="158px" height="90px">
+          <div class="boom-info">
+            <span class="sm-flag musica">Musica</span>
+            <p class="boom-ti">
+              <a href="/musica/otro-boom-nuevo-pa-variar" class="boom-moar">Otro boom nuevo pa variar</a>
+            </p>
+            <a href="#" class="boom-moar">Leer Boom</a>
+          </div>
+          <p class="boom-info-mas">Publicado el <date>Mon, 08 Oct 12</date> - <span>234</span> veces visto - <span>10</span> comments - <span>3</span> modificaciones</p>
+        </li>
+
+        
+        <li class="pags">
+          <ul class="paginador">
+            <li><a href="#"><span class="pagina-prev">prev</span></a></li>
+            <li><a href="#"><span class="pagina">1</span></a></li>
+            <li><a href="#"><span class="pagina even">2</span></a></li>
+            <li><a href="#"><span class="pagina">3</span></a></li>
+            <li><a href="#"><span class="pagina even">4</span></a></li>
+            <li><a href="#"><span class="pagina on">5</span></a></li>
+            <li><a href="#"><span class="pagina even">60</span></a></li>
+            <li><a href="#"><span class="pagina">70</span></a></li>
+            <li><a href="#"><span class="pagina-next">next</span></a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </div>
 
