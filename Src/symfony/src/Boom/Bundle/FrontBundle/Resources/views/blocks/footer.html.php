80eba81931517f33077ba0a3400863bbf0a727c9
Cas <cas@brutalcontent.com>
2012-10-31 12:17:55 -0600

    footer fix

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/blocks/footer.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/blocks/footer.html.php
@@ -1,26 +1,28 @@
 <footer>
-    <div id="f-cont">
-        <ul>
-            <li class="about">
-                <h3><a href="#">Sobre 7Boom</a></h3>
-                <p>7boom es una herramienta de socialización a partir de tu opinión. El espacio donde tú ordenas al mundo como tú quieras. El límite es el siete.</p>
-                <a href="https://www.facebook.com/7boomMx" target="_blank" id="fb-btn">facebook</a>
-                <a href="https://www.twitter.com/7_boom"   target="_blank" id="tw-btn">twitter</a>
-            </li>
-            <li class="contact">
-                <h3><a href="#">Tu marca en 7Boom</a></h3>
-                <p>¿Te interesa anunciarte en 7Boom?</p>
-                <p>Contactanos en <a href="mailto:contacto@7boom.mx">Contacto@7boom.mx</a></p>
-            </li>
-            <li class="newsletter">
-                <h3><a href="#">Recibe nuestro newsletter</a></h3>
-                <form>
-                    <input type="text" name="fullname" placeholder="Nombre completo"/>
-                    <input type="text" name="email" placeholder="Correo electrónico"/>
-                    <input type="submit" value="ENVIAR"/>
-                </form>
-            </li>
-        </ul>
-    </div>
-    <p id="copyright">Copyright © 2012 · All Rights Reserved · <a href="mailto:contacto@7boom.mx">contacto@7boom.mx</a></p>
+  <ul class="f-cont cf">
+    <li class="about">
+      <h3><a href="#">Sobre 7Boom</a></h3>
+      <p>7boom es una herramienta de socialización a partir de tu opinión. El espacio donde tú ordenas al mundo como tú quieras. El límite es el siete.</p>
+      <ul class="bc-site">
+        <li><a href="https://www.facebook.com/7boomMx" target="_blank" class="fb-btn">facebook</a></li>
+        <li><a href="https://www.twitter.com/7_boom"   target="_blank" class="tw-btn">twitter</a></li>
+        <li><p><span>7boom es</span> un sitio de:</p></li>
+        <li><a href="#" class="bc-logo">Brutal Content</a></li>
+      </ul>
+    </li>
+    <li class="contact">
+      <h3><a href="#">Tu marca en 7Boom</a></h3>
+      <p>¿Te interesa anunciarte en 7Boom?</p>
+      <p>Contactanos en <a href="mailto:contacto@7boom.mx">Contacto@7boom.mx</a></p>
+    </li>
+    <li class="newsletter">
+      <h3><a href="#">Recibe nuestro newsletter</a></h3>
+      <form>
+        <input type="text" name="fullname" placeholder="Nombre completo"/>
+        <input type="text" name="email" placeholder="Correo electrónico"/>
+        <input type="submit" value="ENVIAR"/>
+      </form>
+    </li>
+  </ul>
+  <p id="copyright">Copyright © 2012 · All Rights Reserved · <a href="mailto:contacto@7boom.mx">contacto@7boom.mx</a></p>
 </footer>
