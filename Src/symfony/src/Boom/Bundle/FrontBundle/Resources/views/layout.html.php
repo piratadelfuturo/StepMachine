c62264f0b7a37370417b3baa55c74f7403447df4
Daniel M <daniel.maldonado@gmail.com>
2012-11-13 20:40:09 +0400

    Google Analytics

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/layout.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/layout.html.php
@@ -13,7 +13,7 @@
         <?php
         $title = $view->escape($view['slots']->get('title', null));
         ?>
-        <title><?php echo $title !== null ? $title.' - '  : ''; ?>7boom</title>
+        <title><?php echo $title !== null ? $title . ' - ' : ''; ?>7boom</title>
 
         <meta name="description" content="<?php echo $view->escape($view['slots']->get('description', '')) ?>">
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
@@ -27,21 +27,7 @@
           <style type="text/css">.gradient {filter: none;}</style>
         <![endif]-->
 
-        <!-- Use Google CDN for jQuery and jQuery UI -->
         <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/modernizr-1.7.min.js') ?>"></script>
-        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/jquery-1.8.2.min.js') ?>"></script>
-        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/jquery-ui-1.8.23.custom.min.js') ?>"></script>
-        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/file_upload/js/jquery.iframe-transport.js') ?>"></script>
-        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/file_upload/js/jquery.fileupload.js') ?>"></script>
-        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/jquery.cookie.js') ?>"></script>
-        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/jquery.boomAjaxUpload.js') ?>"></script>
-        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/jquery.blockUI.js') ?>"></script>
-        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/tiny_mce/tiny_mce_src.js') ?>"></script>
-        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/jquery.dragsort-0.5.1.min.js') ?>"></script>
-        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/plugins.js') ?>"></script>
-        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/script.js') ?>"></script>
-
-
     </head>
     <body class="gradient">
         <?php
@@ -62,11 +48,32 @@
             //echo $view['actions']->render('BoomFrontBundle:Profile:userBlock', array(), array('standalone' => false));
             echo $view->render('BoomFrontBundle:Profile:blocks/header.html.php');
         }
-
         ?>
         <div id="container" class="<?php $view['slots']->output('layout_container_css_class', '') ?>">
             <?php $view['slots']->output('_content') ?>
         </div>
         <?php echo $view->render('BoomFrontBundle::blocks/footer.html.php'); ?>
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-36293587-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
+        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/jquery-1.8.2.min.js') ?>"></script>
+        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/jquery-ui-1.8.23.custom.min.js') ?>"></script>
+        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/file_upload/js/jquery.iframe-transport.js') ?>"></script>
+        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/file_upload/js/jquery.fileupload.js') ?>"></script>
+        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/jquery.cookie.js') ?>"></script>
+        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/jquery.boomAjaxUpload.js') ?>"></script>
+        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/jquery.blockUI.js') ?>"></script>
+        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/libs/tiny_mce/tiny_mce_src.js') ?>"></script>
+        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/jquery.dragsort-0.5.1.min.js') ?>"></script>
+        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/plugins.js') ?>"></script>
+        <script src="<?php echo $view['assets']->getUrl('/bundles/boomfront/js/script.js') ?>"></script>
     </body>
 </html>
