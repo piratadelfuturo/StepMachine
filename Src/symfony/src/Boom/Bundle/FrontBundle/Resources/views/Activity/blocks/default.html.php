154f1e0869de42b9e9904317c85ebe03c1db66bd
Daniel M <daniel.maldonado@gmail.com>
2012-11-15 03:16:14 +0400

    Espacios

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Activity/blocks/default.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Activity/blocks/default.html.php
@@ -1,8 +1,8 @@
 <li class="add-boom">
     <?php if ($act['self'] === false): ?>
-        <p class="info"><a href="<?php echo $act['profile_url'] ?>"><?php echo $view->escape($act['user']['name']) ?></a> <?php echo $view->escape((string) $entity['data']) ?></p>
+        <p class="info"><a href="<?php echo $act['profile_url'] ?>"> <?php echo $view->escape($act['user']['name']) ?> </a> <?php echo $view->escape((string) $entity['data']) ?></p>
     <?php else: ?>
-        <p class="info"><?php echo $view->escape((string) $entity['data']) ?><a href="<?php echo $act['profile_url'] ?>"><?php echo $view->escape($entity['user']['name']) ?></a>:</p>
+        <p class="info"><?php echo $view->escape((string) $entity['data']) ?> <a href="<?php echo $act['profile_url'] ?>"> <?php echo $view->escape($entity['user']['name']) ?></a>:</p>
     <?php endif; ?>
     <?php if ($entity['boom'] !== null): ?>
         <p class="title">
