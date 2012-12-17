8d490a6ade3e6561e2b2a35653042a07f7c1c0a9
Daniel M <daniel.maldonado@gmail.com>
2012-12-17 13:03:32 -0600

    search

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Default/search.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Default/search.html.php
@@ -4,13 +4,29 @@ $view['slots']->start('top_two_col');
 ?>
 <script>
     function hndlr(response) {
-        console.log(response);
-        for (var i = 0; i < response.items.length; i++) {
-            var item = response.items[i];
-            // in production code, item.htmlTitle should have the HTML entities escaped.
-            document.getElementById("booms-container").innerHTML += "<br>" + item.htmlTitle;
+        if(response.items){
+            var container = $("#booms-container");
+            $.each(response.items,function(i){
+                
+            });
+            .show();
         }
     }
 </script>
-<script src="<?php echo $url ?>?key=<?php echo $key ?>&cx=<?php echo $cx?>&q=<?php echo urlencode($query) ?>&callback=hndlr">
+<script src="<?php echo $url ?>?key=<?php echo $key ?>&cx=<?php echo $cx ?>&q=<?php echo urlencode($query) ?>&callback=hndlr">
 </script>
+<div id="booms-container" style="display:none">
+    <h3 class="title-flag"></h3>
+    <ul class="list cf">
+        <li class="boom on">
+            <img src="/content/boom-img/ee726227/158_90.jpeg" width="158px" height="90px">
+            <div class="boom-info">
+                <span class="sm-flag"></span>
+                <p class="boom-ti">
+                    <a href="/musica/los-mejores-discos-de-2012" class="boom-moar"></a>
+                    <span></span>
+                </p>
+            </div>
+        </li>
+    </ul>
+</div>
