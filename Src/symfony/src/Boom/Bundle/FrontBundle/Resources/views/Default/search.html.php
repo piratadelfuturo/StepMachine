4e04859b6ab9286a7de482b00eaa64834bdde972
Eder <eder@brutalcontent.com>
2012-12-17 13:02:21 -0600

    prueba de markup de busqueda

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Default/search.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Default/search.html.php
@@ -3,14 +3,27 @@ $view->extend('BoomFrontBundle::two_col_sublayout.html.php');
 $view['slots']->start('top_two_col');
 ?>
 <script>
-    function hndlr(response) {
-        console.log(response);
-        for (var i = 0; i < response.items.length; i++) {
-            var item = response.items[i];
-            // in production code, item.htmlTitle should have the HTML entities escaped.
-            document.getElementById("booms-container").innerHTML += "<br>" + item.htmlTitle;
-        }
-    }
+function hndlr(response) {
+  var contentDiv = document.getElementById("booms-container");
+
+  contentDiv.innerHTML = '';
+  console.log(response);
+
+  var newResultsDiv = document.createElement('div');
+  newResultsDiv.id = 'booms-container';
+
+  for (var i = 0; i < response.items.length; i++) {
+    var item = response.items[i];
+
+    var resultHTML = '<div class="boom">';
+    resultHTML += '<a href="' + item.formattedUrl + '">' + item.pagemap['cse_thumbnail'] + '</a><div class="boom-info"<p class="boom-ti"><a href="'
+      + item.formattedUrl + '">' + item.title + '</a></p><p class="src-snip">' + item.snippet + '</p></div></div>';
+
+    newResultsDiv.innerHTML += resultHTML;
+  }
+  contentDiv.appendChild(newResultsDiv);
+}
+
 </script>
 <script src="<?php echo $url ?>?key=<?php echo $key ?>&cx=<?php echo $cx?>&q=<?php echo urlencode($query) ?>&callback=hndlr">
 </script>
