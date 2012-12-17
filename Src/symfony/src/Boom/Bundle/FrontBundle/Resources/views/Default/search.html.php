a783ebac6b6b2190fdc60c1d53ab11e04f9616e9
Eder <eder@brutalcontent.com>
2012-12-17 17:41:54 -0600

    otra corrección

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Default/search.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/Default/search.html.php
@@ -18,7 +18,7 @@ function hndlr(response) {
 
     var resultHTML = '<div class="boom">';
     if( typeof(item.pagemap.cse_thumbnail) == "object" ) {
-      resultHTML += '<a href="http://' + item.formattedUrl + '"><img src="' + item.pagemap.cse_thumbnail[0].src + '"/></a><div class="boom-info"><p class="boom-ti"><a href="http://'
+      resultHTML += '<a href="http://' + item.formattedUrl + '"><img src="' + item.pagemap.cse_thumbnail[0].src + '"/></a><div class="boom-info"><p class="boom-ti cf"><a href="http://'
         + item.formattedUrl + '">' + item.title + '</a></p><p class="src-snip">' + item.snippet + '</p></div></div>';
     } else {
       resultHTML += '<div class="boom-info"<p class="boom-ti"><a href="http://'
