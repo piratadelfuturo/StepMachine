5cfdfd6de1fa0a75afc3df62a01379fc8b1e1e34
Eder <eder@brutalcontent.com>
2012-12-18 13:30:02 -0600

    correcciones a boom patrocinado

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/public/especiales/7boomytu/js/script.js
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/public/especiales/7boomytu/js/script.js
@@ -1,34 +1,46 @@
 $(document).ready(function(){
+
+  //Side scrolling
+  $(function() {
+    $("body").mousewheel(function(event, delta) {
+      this.scrollLeft -= (delta * 30);
+      event.preventDefault();
+    });
+  });
+
   var isfirst = parseInt("0");
   var pageX = function(){
     return $(window).scrollLeft();
   }
-  
+
   $('.bloque').addClass('hide');
   $('.numero').addClass('hide');
-  
+
   var scrollTimeout = null;
   var scrollendDelay = 100;
+
   function scrollbeginHandler() {
     $('.dude-piernas').addClass('walk');
   }
+
   function scrollendHandler() {
     $('.dude-piernas').removeClass('walk');
   	scrollTimeout = null;
   }
+
   $(function() {
-  	$(window).scroll(function() {
-  	  if ( scrollTimeout === null ) {
-  		  scrollbeginHandler();
-  		}else{
-  		  clearTimeout( scrollTimeout );
-  		}
-  		scrollTimeout = setTimeout( scrollendHandler, scrollendDelay );
-  		});
-  	});
+    $(window).scroll(function() {
+      if ( scrollTimeout === null ) {
+        scrollbeginHandler();
+      }else{
+        clearTimeout( scrollTimeout );
+      }
+      scrollTimeout = setTimeout( scrollendHandler, scrollendDelay );
+    });
+  });
 
   $(window).scroll(function(){
-    
+
     //tirar geocities
     if ( pageX() >= 1 ) {
       $('#dude-banner').addClass('jump');
@@ -38,109 +50,108 @@ $(document).ready(function(){
     else {
       $('#geocities').removeClass('hideGC').delay(2800).show(0);
     };
-    
+
     //agregar left a texto y a dude
-    $('#txt-wrapper').css('left', (pageX() + 280) + 'px');
-    $('#dude').css('left', (pageX() + 350) + 'px');
-    $('#dude2').css('left', (pageX() + 500) + 'px'); 
+    $('#dude2').css('left', (pageX() + 500) + 'px');
+
     //triggers texto
     if ( pageX() >= 400 ){
       $('#txt1 .numero').removeClass('hide').addClass('show');
       $('#txt1 .uno').removeClass('hide').addClass('show');
       $('#txt1 .dos').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 800 ){
       $('#txt1 .uno').removeClass('show').addClass('hide');
       $('#txt1 .dos').removeClass('hide').addClass('show');
       $('#txt1 .tres').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 1100 ){
       $('#txt1 .dos').removeClass('show').addClass('hide');
       $('#txt1 .tres').removeClass('hide').addClass('show');
       $('#txt1 .cuatro').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 1500 ){
       $('#txt1 .tres').removeClass('show').addClass('hide');
       $('#txt1 .cuatro').removeClass('hide').addClass('show');
     };
-    
+
     if ( pageX() >= 1800 ){
       $('#txt1 .cuatro').removeClass('show').addClass('hide');
       $('#txt1 .numero').removeClass('show').addClass('hide');
       $('#txt2 .numero').removeClass('show').addClass('hide');
       $('#txt2 .uno').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 2500 ){
       $('#txt2 .numero').removeClass('hide').addClass('show');
       $('#txt2 .uno').removeClass('hide').addClass('show');
       $('#txt2 .dos').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 2800 ){
       $('#txt2 .uno').removeClass('show').addClass('hide');
       $('#txt2 .dos').removeClass('hide').addClass('show');
       $('#txt2 .tres').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 3400 ){
       $('#txt2 .dos').removeClass('show').addClass('hide');
       $('#txt2 .tres').removeClass('hide').addClass('show');
     };
-    
+
     if ( pageX() >= 3800 ){
       $('#txt2 .tres').removeClass('show').addClass('hide');
       $('#txt2 .numero').removeClass('show').addClass('hide');
       $('#txt3 .uno').removeClass('show').addClass('hide');
       $('#txt3 .numero').removeClass('show').addClass('hide');
-    };    
-    
+    };
+
     if ( pageX() >= 4600 ){
       $('#txt3 .numero').removeClass('hide').addClass('show');
       $('#txt3 .uno').removeClass('hide').addClass('show');
     };
-    
+
     if ( pageX() >= 4750 ){
       $('#txt3 .dos').removeClass('show').addClass('hide');
       $('#txt3 .uno').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 5000 ){
       $('#txt3 .dos').removeClass('hide').addClass('show');
       $('#txt3 .tres').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 5200 ){
       $('#txt3 .tres').removeClass('hide').addClass('show');
     };
-    
+
     if ( pageX() >= 5500 ){
       $('#txt3 .dos').removeClass('show').addClass('hide');
       $('#txt3 .tres').removeClass('show').addClass('hide');
       $('#txt3 .numero').removeClass('show').addClass('hide');
       $('#txt4 .uno').removeClass('show').addClass('hide');
       $('#txt4 .numero').removeClass('show').addClass('hide');
-    }; 
-    
+    };
+
     if ( pageX() >= 5700 ){
       $('#txt4 .numero').removeClass('hide').addClass('show');
       $('#txt4 .uno').removeClass('hide').addClass('show');
       $('#txt4 .dos').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 5900 ){
       $('#txt4 .uno').removeClass('show').addClass('hide');
       $('#txt4 .dos').removeClass('hide').addClass('show');
       $('#txt4 .tres').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 6100 ){
       $('#txt4 .tres').removeClass('hide').addClass('show');
     };
-    
+
     if ( pageX() >= 6800 ){
       $('#txt4 .dos').removeClass('show').addClass('hide');
       $('#txt4 .tres').removeClass('show').addClass('hide');
@@ -148,30 +159,30 @@ $(document).ready(function(){
       $('#txt5 .uno').removeClass('show').addClass('hide');
       $('#txt5 .numero').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 7000 ){
       $('#txt5 .numero').removeClass('hide').addClass('show');
       $('#txt5 .uno').removeClass('hide').addClass('show');
       $('#txt5 .dos').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 7300 ){
       $('#txt5 .uno').removeClass('show').addClass('hide');
       $('#txt5 .dos').removeClass('hide').addClass('show');
       $('#txt5 .tres').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 7500 ){
       $('#txt5 .tres').removeClass('hide').addClass('show');
       $('#txt5 .cuatro').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 7700 ){
       $('#txt5 .dos').removeClass('show').addClass('hide');
       $('#txt5 .tres').removeClass('show').addClass('hide');
       $('#txt5 .cuatro').removeClass('hide').addClass('show');
     };
-    
+
     if ( pageX() >= 8200 ){
       $('#txt5 .numero').removeClass('show').addClass('hide');
       $('#txt5 .cuatro').removeClass('show').addClass('hide');
@@ -179,36 +190,36 @@ $(document).ready(function(){
       $('#txt6 .numero').removeClass('show').addClass('hide');
       $('#txt6 .uno').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 8500 ){
       $('#txt6 .numero').removeClass('hide').addClass('show');
       $('#txt6 .uno').removeClass('hide').addClass('show');
       $('#txt6 .dos').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 8600 ){
       $('#txt6 .dos').removeClass('hide').addClass('show');
       $('#txt6 .tres').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 8800 ){
       $('#txt6 .tres').removeClass('hide').addClass('show');
       $('#txt6 .dos').removeClass('show').addClass('hide');
       $('#txt6 .uno').removeClass('show').addClass('hide');
       $('#txt6 .cuatro').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 9100 ){
       $('#txt6 .cuatro').removeClass('hide').addClass('show');
       $('#txt6 .tres').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 9350 ){
       $('#txt6 .cuatro').removeClass('show').addClass('hide').delay(5600).queue(function(next){
         $('#txt6 .cinco').removeClass('hide').addClass('show');
-      });     
+      });
     };
-    
+
     if ( pageX() >= 9800 ){
       $('#txt6 .numero').removeClass('show').addClass('hide');
       $('#txt6 .cinco').removeClass('show').addClass('hide');
@@ -217,17 +228,15 @@ $(document).ready(function(){
       $('#txt7 .dos').removeClass('show').addClass('hide');
       $('#txt7 .tres').removeClass('show').addClass('hide');
     };
-    
+
     if ( pageX() >= 10700 ){
       $('#txt7 .numero').removeClass('hide').addClass('show');
       $('#txt7 .uno').removeClass('hide').addClass('show');
     };
-    
-    
+
     //triggers animaciones escena 3
-    
     $('#globo').css('left', (pageX() + 400) + 'px');
-    
+
     if ( pageX() >= 4800 ){
       $('#nube').removeClass('hide').addClass('show');
       };
@@ -268,16 +277,16 @@ $(document).ready(function(){
     if ( pageX() >= 8850 ){ 
       $('#dude .dude-casco').removeClass('hide').addClass('show');
     };
-    
+
     if ( pageX() >= 9100 ){ 
       $('#dude').removeClass('jump');
       $('#cohete #cohete-bubble').removeClass('hide').addClass('show');
     };
-    
+
     if ( pageX() >= 9100 ){ 
       $('#dude').hide(0);
     };
-    
+
     if ( pageX() >= 9350){ 
       if(isfirst == 0){
         $('#cohete').addClass('volar').delay(3000).queue(function(next){
@@ -293,25 +302,22 @@ $(document).ready(function(){
         $('#dude2').delay(2000).queue(function(next){
             $(this).removeClass('feliz').stop();
             next();
-        });  
+        });
         $('#dude2 .dude-parachute').removeClass('hide').addClass('show').delay(5600).queue(function(next){
           $(this).removeClass('show').addClass('hide').stop();
-        });   
+        });
 	isfirst = 1;
       }
-      
+
     };
-    
+
     if ( pageX() >= 11100 ){
       $('#txt7 .uno').removeClass('show').addClass('hide');
       $('#txt7 .dos, #txt7 .tres').removeClass('hide').addClass('show');
       $('#dude2 .b-left').addClass('feliz');
       $('#social').removeClass('hidden').addClass('appear');
     };
-    
-    
+
   });
 
-  
-  
 });
