a9c4bde2c7a881cf22bf7911ee8504bfa69b7cc8
Tannia <tannia.obregon@gmail.com>
2013-01-17 16:16:04 -0600

    textos terminos y privacidad

--- Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/blocks/footer.html.php
+++ Src/symfony/src/Boom/Bundle/FrontBundle/Resources/views/blocks/footer.html.php
@@ -47,7 +47,46 @@
               <div>
                 <h3>Términos y Condiciones</h3>
                 <p>
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                7Boom es propiedad de Medios Brutales S.A de C.V, con domicilio en Tiburcio Montiel 96-A, Col. San Miguel Chapultepec y al momento de utilizar 7boom, el usuario acepta los términos y condiciones que se describen a continuación:
+                </p>
+                <p>
+                <span>Aviso de Privacidad.</span> En cumplimiento a lo establecido en los artículos 15 y 16 de la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, 7boom informa en este acto y el titular de los datos acepta, que la información que se recaba tiene como fin y está destinada a: (i) proveer servicios personalizados de contenido, (ii) informar sobre servicios relacionados y adquiridos por el usuario, (iii) facilitar promociones que requieran datos del usuario, (iv) evaluar la calidad del servicio brindado y (v) como material para estudios internos sobre el comportamiento del usuario en la plataforma de 7Boom. 
+                </p>
+                <p>
+                El usuario acepta que los datos proporcionados, incluidos, correo electrónico, edad, Login directo a Facebook (en caso de accesar datos con correo electrónico), Fotografía del usuario (que podrá ser cambiada por el mismo en cualquier momento), así como también  Password y autenticación (que estará cifrado), serán almacenados por 7boom y podrán ser utilizados de acuerdo a lo establecido en los presentes Términos y Condiciones y en la Política de Privacidad.
+                </p>
+                <p>
+                <span>Política de privacidad.</span> 7boom (i) No tiene ningún tipo de responsabilidad por las decisiones a las que arribe el receptor de la información ni por los daños que pudiera ocasionar la falta de exactitud de los datos asentados, (ii) se reserva el derecho de anular los comentarios que se consideren falsos o tengan intensiones que de alguna manera favorezcan o perjudiquen con alevosía a un establecimiento en particular. y (iii) se reserva el derecho de:
+                </p>
+                <p>
+                <span>Contenidos:</span> Los contenidos que se exhiben en este sitio son de exclusiva propiedad de 7Boom y se encuentra prohibida su reproducción total y/o parcial. 
+                </p>
+                <p>
+                Los datos que se publican están para ser consultados por el usuario final y se prohíbe la descarga por cualquier medio de los datos recabados y su posterior utilización con cualquier fin.
+                </p>
+                <p>
+                En algunos casos, queda expresamente prohibido el acceso y/o utilización del servicio por menores de 18 años, y en caso de violación a esta prohibición la responsabilidad por el acceso y/o utilización del servicio por parte de los menores de edad corresponderá a los mayores a cuyo cargo se encuentren, sea por Patria Potestad o por tutela.
+                </p>
+                <p>
+                <span>Comentarios:</span> Los usuarios autorizan a 7boom a publicar, distribuir, ceder y hacer uso de los comentarios y votos que ingresan al sistema, renunciando a cualquier derecho de autor que pueda recaer sobre los mismos. Los Usuarios son responsables por los comentarios y votos, debiendo mantener indemne a 7boom ante posibles reclamos por ellos generados. Se prohíbe la utilización de términos ofensivos y contrarios a la moral y buenas costumbres. No se aceptarán comentarios a modo de denuncia (ya sean sobre higiene, salubridad o impositivas) ni comentarios que mencionen nombres de personas o nombres terceros distintos al que se está comentando. 7boom se reserva el derecho de no publicar, editar o borrar aquellos comentarios que a su entender violen las disposiciones de la presente política.
+                </p>
+                <p>
+                El Usuario expresamente reconoce y acepta que: (i) no está permitido utilizar lenguaje vulgar /obsceno, discriminatorio y/u ofensivo, (ii) está prohibido todo acto contrario a las leyes, moral y buenas costumbres, (iii) está prohibido publicar mensajes agraviante, difamatorios, calumniosos, injuriosos, falsos, dilacerantes, ofensivos, discriminatorios, pornográficos, de contenido violento, insultantes, amenazantes, instigantes a conductas de contenido ilícito o peligrosas para la salud, y la utilización de lenguaje inapropiado y/o que vulneren de cualquier forma la privacidad de cualquier tercero, (iv) está prohibido violar directa o indirectamente los derechos intelectuales de propiedad de cualquier persona, cualquier copyright, marcas, derechos de publicidad, u otros derechos de propiedad, (v) está prohibido promocionar, comercializar, vender, publicar y/u ofrecer cualquier clase de productos, servicios y/o actividades por intermedio de o a través de la utilización del servicio incluyendo cualquier tipo de propaganda o publicidad encubierta y (vi) está prohibido la publicación de datos personales sin el consentimiento expreso de la persona involucrada.
+                </p>
+                <p>
+                7boom se reserva el derecho de no publicar (o remover luego de ser publicados) todos aquellos contenidos y/o mensajes propuestos y/o publicados por el Usuario que 7boom considere que, no respondan estrictamente a las disposiciones contenidas en el presente y/o resulten impropios y/o inadecuados a las características, finalidad y/o calidad del servicio, asimismo 7Boom no es en ningún caso responsable de la destrucción, alteración o eliminación del contenido o información que cada Usuario incluya en sus mensajes.
+                </p>
+                <p>
+                7Boom no garantiza ningún plazo de publicación de los comentarios, ni disponibilidad y continuidad del funcionamiento del servicio.
+                </p>
+                <p>
+                Cada usuario es único y exclusivo responsable de sus manifestaciones, dichos, opiniones y todo acto que realice, debiendo mantener indemne a 7boom por todo daño que genere, ya sea en forma directa o por terceros que le realicen reclamos.
+                </p>
+                <p>
+                El Usuario reconoce los derechos para la publicación reproducción, o cesión a terceros por cualquier título, de los contenidos con los que hubiera participado. La publicación de un mensaje, comentario o cualquier otra forma de participación de un usuario, en el sitio de 7Boom, no generará ninguna obligación económica para con el usuario, por parte de 7Boom.
+                </p>
+                <p>
+                <span>Jurisdicción y competencia.</span> Para todas las cuestiones litigiosas que se susciten en relación con la interpretación, ejecución y cumplimiento del presente acuerdo, las partes se someten a la jurisdicción y leyes aplicables de los Tribunales de México, Distrito Federal, renunciando a cualquier otra jurisdicción u ordenamiento jurídico que por cualquier motivo pudiese corresponderles.
                 </p>
               </div>
             </div>
@@ -59,7 +98,10 @@
               <div>
                 <h3>Aviso de Privacidad</h3>
                 <p>
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                 En cumplimiento a lo establecido en los artículos 15 y 16 de la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, 7boom informa en este acto y el titular de los datos acepta, que la información que se recaba tiene como fin y está destinada a: (i) proveer servicios personalizados de contenido, (ii) informar sobre servicios relacionados y adquiridos por el usuario, (iii) facilitar promociones que requieran datos del usuario, (iv) evaluar la calidad del servicio brindado y (v) como material para estudios internos sobre el comportamiento del usuario en la plataforma de 7Boom. 
+                </p>
+                <p>
+                El usuario acepta que los datos proporcionados, incluidos, correo electrónico, edad, Login directo a Facebook (en caso de accesar datos con correo electrónico), Fotografía del usuario (que podrá ser cambiada por el mismo en cualquier momento), así como también  Password y autenticación (que estará cifrado), serán almacenados por 7boom y podrán ser utilizados de acuerdo a lo establecido en los presentes Términos y Condiciones y en la Política de Privacidad.
                 </p>
               </div>
             </div>
