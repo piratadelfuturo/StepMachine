9093368c6796831cd8b10511c4b62f0690502519
Daniel M <daniel.maldonado@gmail.com>
2013-01-25 13:03:59 -0500

    Fix de forma de reset

--- Src/symfony/src/Boom/Bundle/UserBundle/Resources/views/Resetting/reset_content.html.php
+++ Src/symfony/src/Boom/Bundle/UserBundle/Resources/views/Resetting/reset_content.html.php
@@ -1,4 +1,6 @@
-<?php $view->extend('BoomFrontBundle::two_col_sublayout.html.php') ?>
+<?php
+$view->extend('BoomFrontBundle::two_col_sublayout.html.php');
+?>
 <div class="pass-page">
   <h3 class="short-bar">Recuperar Password</h3>
   <p class="lab">Ok, ya vimos que no te gustó tu antigua contraseña, <br />
@@ -7,10 +9,10 @@
       <?php echo $view['form']->enctype($form) ?> method="POST" class="fos_user_resetting_reset">
         <?php echo $view['form']->widget($form['_token']); ?>
       <div class="grad-border">
-        <?php echo $view['form']->widget($form['new']['first'], array('attr' => array( 'class' => 'cp-new', 'placeholder' => 'Introduce tu nueva contraseña...' ) )); ?>
+        <?php echo $view['form']->widget($form['plainPassword']['first'], array('attr' => array( 'class' => 'cp-new', 'placeholder' => 'Introduce tu nueva contraseña...' ) )); ?>
       </div>
       <div class="grad-border">
-        <?php echo $view['form']->widget($form['new']['second'], array('attr' => array( 'class' => 'cp-confirm', 'placeholder' => 'Una vez más, para estar seguros...' ) )); ?>
+        <?php echo $view['form']->widget($form['plainPassword']['second'], array('attr' => array( 'class' => 'cp-confirm', 'placeholder' => 'Una vez más, para estar seguros...' ) )); ?>
       </div>
       <div class="sub-btn">
           <input type="submit" value="Reset" />
