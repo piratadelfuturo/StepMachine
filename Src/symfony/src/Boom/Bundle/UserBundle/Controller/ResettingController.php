65b3ddbc9c9e2d24797b1b035eeefcceff29aac5
Daniel M <daniel.maldonado@gmail.com>
2013-01-25 12:51:02 -0500

    Fix de reset

--- Src/symfony/src/Boom/Bundle/UserBundle/Controller/ResettingController.php
+++ Src/symfony/src/Boom/Bundle/UserBundle/Controller/ResettingController.php
@@ -79,7 +79,7 @@ class ResettingController extends BaseController {
     /**
      * Reset user password
      */
-    public function resetAction(Request $request, $token) {
+    public function resetActionBk(Request $request, $token) {
         $user = $this->container->get('fos_user.user_manager')->findUserByConfirmationToken($token);
 
         if (null === $user) {
@@ -102,6 +102,61 @@ class ResettingController extends BaseController {
             $this->authenticateUser($user, $response);
 
             return $response;
+        }
+
+        return $this->container->get('templating')->renderResponse('', array(
+                    'token' => $token,
+                    'form' => $form->createView(),
+                ));
+    }
+
+    /**
+     * Reset user password
+     */
+    public function resetAction(Request $request, $token) {
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->container->get('fos_user.resetting.form.factory');
+        /** @var $userManager \FOS\UserBundle\Model\UserManagerInterface */
+        $userManager = $this->container->get('fos_user.user_manager');
+        /** @var $dispatcher \Symfony\Component\EventDispatcher\EventDispatcherInterface */
+        $dispatcher = $this->container->get('event_dispatcher');
+
+        $user = $userManager->findUserByConfirmationToken($token);
+
+        if (null === $user) {
+            throw new NotFoundHttpException(sprintf('The user with "confirmation token" does not exist for value "%s"', $token));
+        }
+
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::RESETTING_RESET_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+        if ('POST' === $request->getMethod()) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $event = new FormEvent($form, $request);
+                $dispatcher->dispatch(FOSUserEvents::RESETTING_RESET_SUCCESS, $event);
+
+                $userManager->updateUser($user);
+
+                if (null === $response = $event->getResponse()) {
+                    $this->setFlash('fos_user_success', 'resetting.flash.success');
+                    //$response = new RedirectResponse($this->getRedirectionUrl($user));
+                    $response = $this->container->get('templating')->renderResponse('BoomUserBundle:Resetting:success.html.php');
+                    $this->authenticateUser($user, $response);
+                }
+
+                $dispatcher->dispatch(FOSUserEvents::RESETTING_RESET_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+                return $response;
+            }
         }
 
         return $this->container->get('templating')->renderResponse('BoomUserBundle:Resetting:reset_content.html.php', array(
